0e02c168162ca74a0c513aadb1b278e0d2180c7e
clancy <clancy_lin@163.com>
2019-08-04 17:59:01 +0800

    Create:  进度条插件              评论插件              ts介绍文章 Update: 更新代码架构               导航栏

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,29 +1,35 @@
 module.exports = {
   title: 'Clancy小屋',
-  description: 'Clancy 的博客',
+  description: '随你肆意颤抖',
   base: '/',
   dest: 'dist/',
   theme: 'ououe',
+  head: [
+    ['link', { rel: 'icon', href: `/public/icon.ico` }]
+  ],
   themeConfig: {
     defaultTheme: 'light',
-    cover: '/cover.jpg',
+    cover: '/cover2.jpg',
     logo: '/logo.png',
     search: true,
+    useVssue: true,
     pageGroup: 5,
     postTime: {
-      createTime: 'Create Time',
-      lastUpdated: 'Last Updated'
+      createTime: '创建时间',
+      lastUpdated: '最后修改'
     },
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Posts', link: '/posts/' },
-      { text: 'Doc', link: '/doc/' },
-      { text: 'Tags', link: '/tag/' },
-      { text: 'Categories', link: '/category/' },
-      { text: 'About', link: '/about/' }
+      { text: '首页', link: '/' },
+      { text: '教育技术', link: '/IT/' },
+      { text: '前端', link: '/Front/' },
+      { text: '杂谈', link: '/Comment/' },
+      { text: 'TypeScript', link: '/TS/' },
+      { text: 'Rust', link: '/Rust/' },
+      { text: '分类', link: '/category/' },
+      { text: '关于', link: '/About/' }
     ],
     footer: [
-      { text: 'Github', link: 'https://github.com/tolking' }
+      { text: 'Github', link: 'https://github.com/linsicong003' }
     ]
   },
   postcss: {
@@ -52,13 +58,25 @@ module.exports = {
       type: 'danger',
       before: info => `<div class="danger"><p class="title">${info}</p>`,
       after: '</div>'
+    }], 
+    'reading-progress',
+    ['@vssue/vuepress-plugin-vssue', {
+      // set `platform` rather than `api`
+      platform: 'github',
+
+      // all other options of Vssue are allowed
+      owner: 'Clancy',
+      repo: 'https://github.com/linsicong003/myBlog',
+      clientId: 'f91ff9a1205ad9ac7c60',
+      clientSecret: 'c4fd8f99a68ae9742e96dd8824e48e6cc9a36018',
     }]
   ],
   configureWebpack: {
     resolve: {
       alias: {
         '@alias': 'path/to/some/dir',
-        '@img': 'public/image'
+        '@img': 'public/image',
+        '@public': 'public'
       }
     }
   }
